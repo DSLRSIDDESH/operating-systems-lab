214f371728a516252e88182df7fc733ff8a7d641
Siddesh Devarakonda <siddeshdslr@gmail.com>
2023-11-10 10:00:46 +0530

    Shared Memory

--- lab-06/receiver.c
+++ lab-06/receiver.c
@@ -1,10 +1,4 @@
 // CS21B2019 DEVARAKONDA SLR SIDDESH
-
-// Write two different programs (Sender and Receiver) in C to demonstrate IPC using shared memory. 
-// Process "Sender.c" will write atleast 100 bytes of data to the shared memory and process "Receiver.c"
-//  will read from the shared memory and displays it. Then, "Receiver.c" will write atleast 100 bytes of data 
-//  to the same shared memory and  "Sender.c" will read and displays it. 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
